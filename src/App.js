e9db4bb0cc6a72431ee6e3f13efeac9fbb892a69
Vedant Farde <110126125+VKFarde@users.noreply.github.com>
2022-09-25 15:39:33 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { BrowserRouter as Router,Switch, Route, Link } from 'react-router-dom';
+import { Layout, Typography, Space } from 'antd';
+
+import { Exchanges, Homepage, News, Cryptocurrencies, CryptoDetails, Wallet, Navbar } from './components';
+import './App.css';
+
+const App = () => (
+  <div className="app">
+    <div className="navbar">
+      <Navbar />
+    </div>
+    <div className="main">
+      <Layout>
+        <div className="routes">
+          <Switch>
+            <Route exact path="/">
+              <Homepage />
+            </Route>
+            <Route exact path="/exchanges">
+              <Exchanges />
+            </Route>
+            <Route exact path="/cryptocurrencies">
+              <Cryptocurrencies />
+            </Route>
+            <Route exact path="/crypto/:coinId">
+              <CryptoDetails />
+            </Route>
+            <Route exact path="/news">
+              <News />
+            </Route>
+            <Route exact path="/wallet">
+              <Wallet />
+            </Route>
+          </Switch>
+        </div>
+      </Layout>
+      <div className="footer">
+        <Typography.Title level={5} style={{ color: 'white', textAlign: 'center' }}>Copyright © 2021
+          <Link to="/">
+            SectorX Inc.
+          </Link> <br />
+          All Rights Reserved.
+        </Typography.Title>
+        <Space>
+          <Link to="/">Home</Link>
+          <Link to="/exchanges">Exchanges</Link>
+          <Link to="/news">News</Link>
+          <Link to="/wallet">Wallet</Link>
+        </Space>
+      </div>
+    </div>
+  </div>
+);
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import * as ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import App from './App';
+import 'antd/dist/antd.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Router>
+        <App />
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
